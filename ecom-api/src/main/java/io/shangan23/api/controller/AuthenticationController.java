64e93f2513de68ae46345bcdafed47e36375fcee
Shankar <shangan.23@gmail.com>
2020-11-30 19:50:58 +0530

    feat(crossOrigin) - cross origin support added to API

--- ecom-api/src/main/java/io/shangan23/api/controller/AuthenticationController.java
+++ ecom-api/src/main/java/io/shangan23/api/controller/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import io.shangan23.api.util.JwtUtil;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class AuthenticationController {
 
 	@Autowired
